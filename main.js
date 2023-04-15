da932b5c332dd7af8a395f745ec3b53ebca88d1d
matthiasjrichter <draussensaiter@gmail.com>
2023-04-15 14:40:00 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -109,3 +109,22 @@ function grow(x){
 
 // as arrow function
 const grow = (x) => x.reduce((acc, c) => acc * c, 1)
+
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+// 
+// Examples
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+// don't worry about uppercase vowels
+// y is not considered a vowel for this kata
+
+function shortcut (string) {
+  return string.replaceAll("a","").replaceAll("e","").replaceAll("i","").replaceAll("o","").replaceAll("u","")
+}
+shortcut("HEllo")
+
+//as arrow function:
+
+const shortcut = string => string.replaceAll("a","").replaceAll("e","").replaceAll("i","").replaceAll("o","").replaceAll("u","")

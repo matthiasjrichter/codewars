5d78781c6f1b646bfa3d6d8a87650569cf006b4e
matthiasjrichter <draussensaiter@gmail.com>
2023-04-16 17:22:28 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -140,3 +140,5 @@ function hoopCount (n) {
   if(n >= 10) return  "Great, now move on to tricks"
 else return "Keep at it until you get it"
 }
+//alternative with conditional (ternary) operator and arrow function
+const hoopCount = n => n < 10 ? 'Keep at it until you get it' : 'Great, now move on to tricks';

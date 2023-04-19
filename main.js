ab5b177bc6b2cbc4cafa02ee72d8ffcae1445e85
matthiasjrichter <draussensaiter@gmail.com>
2023-04-19 03:02:55 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -161,3 +161,17 @@ function greet (name, owner) {
 }
 //alternative:
 const greet = (name,owner) => name == owner ? "Hello boss" : "Hello guest"
+
+
+// You are given two interior angles (in degrees) of a triangle.
+// 
+// Write a function to return the 3rd.
+// 
+// Note: only positive integers will be tested.
+
+function otherAngle(a, b) {
+  return 180 - a - b
+}
+//arrow function:
+const otherAngle = (a, b) => 180 - a - b
+otherAngle(40,40)

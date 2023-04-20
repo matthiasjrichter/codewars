f519d9745a56f0a4418a670c9ed892200b9181da
matthiasjrichter <draussensaiter@gmail.com>
2023-04-20 05:49:10 +0200

    daily update to main.js

--- main.js
+++ main.js
@@ -175,3 +175,18 @@ function otherAngle(a, b) {
 //arrow function:
 const otherAngle = (a, b) => 180 - a - b
 otherAngle(40,40)
+
+//Make a simple function called greet that returns the most-famous "hello world!".
+//
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+function greet(){
+  if(2 + 2 === 5){
+    alert ("Big Brother is Watching You.")
+    return "War is Peace, Freedom is Slavery, Ignorance is Strength."
+  }else{
+    return "hello world!"
+  }
+}
+greet()

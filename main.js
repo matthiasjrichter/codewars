1d72c39333a71d13331e53a7516ac42e52d1e3f2
matthiasjrichter <draussensaiter@gmail.com>
2023-04-23 13:26:04 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -240,3 +240,20 @@ sumStr(20,30)
 //arrow function:
 const sumStr = (a,b) => String(Number(a) + Number(b))
 sumStr(20,30)
+
+// Return the number (count) of vowels in the given string.
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  strArr = str.split("")
+  const result = strArr.filter(c => c==="a" ||c==="e" ||c==="i" ||c==="o" || c==="u")
+  return result.length
+}
+
+//shorter:
+function getCount(str) {
+  return str.split("").filter(c => c === "a" || c === "e" || c === "i" || c === "o" || c === "u").length 
+}
+
+

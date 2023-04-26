181b75902de7693074b4dec8ea093eb705a3c23a
matthiasjrichter <draussensaiter@gmail.com>
2023-04-26 16:04:26 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -308,3 +308,11 @@ function finalGrade (exam, projects) {
     return 75
   }else return 0
 }
+//ternary:
+const finalGrade = (exam, projects) => {
+  return (
+    exam > 90 || projects > 10 ? 100 :
+    exam > 75 && projects >= 5 ? 90 :
+    exam > 50 && projects >= 2 ? 75 : 0
+  )
+}

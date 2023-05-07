32a4a60664b9e40b0ccec3428f6cae22f6160abd
matthiasjrichter <draussensaiter@gmail.com>
2023-05-07 16:51:39 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -483,3 +483,29 @@ function digitize(n) {
                   .reverse() //reverse array
                   .map(Number) //make elements numbers again
 }
+
+
+// Given an array of integers.
+// 
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+// 
+// If the input is an empty array or is null, return an empty array.
+// 
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+  if (input == null || input.length == 0) {
+    return [];
+  }
+
+  let negativesArr = input.filter(x => x < 0);
+  let positivesArr = input.filter(x => x > 0);
+
+  let positives = positivesArr.length;
+  let negatives = negativesArr.reduce((acc, cur) => acc + cur, 0);
+  
+  let result = new Array(positives, negatives);
+  return result;
+}
+// OMG this is so messy, sorry!

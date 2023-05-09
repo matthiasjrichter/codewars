ecee45c029f4d607bce7bf035576a82027b2f4c9
matthiasjrichter <draussensaiter@gmail.com>
2023-05-09 15:46:01 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -521,3 +521,14 @@ function countPositivesSumNegatives(input) {
 function move (position, roll) {
   return position + 2 * roll
 }
+
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+// 
+// Note: input will never be an empty string
+
+function fakeBin(x){
+return x.split("").map((e) => e < 5? 0 : 1).join("")
+}
+//short:
+const fakeBin = x => x.split('').map((e) => e < 5 ? 0 : 1).join('')

a9af78ac4eab32726f5d9b7653c02b8b13dd7313
matthiasjrichter <draussensaiter@gmail.com>
2023-05-10 19:42:03 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -532,3 +532,26 @@ return x.split("").map((e) => e < 5? 0 : 1).join("")
 }
 //short:
 const fakeBin = x => x.split('').map((e) => e < 5 ? 0 : 1).join('')
+
+
+
+// Write a small function that returns the values of an array that are not odd.
+// 
+// All values in the array will be integers. Return the good values in the order they are given.
+function noOdds(values){
+  return values.filter( (e) => e % 2 === 0)
+}
+//or
+const noOdds = values => values.filter( (e) => e % 2 === 0)
+//or without filter
+function noOdds(arr){
+  let evens = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 === 0) {
+      evens.push(arr[i]);
+    }
+  }
+  
+  return evens;
+}

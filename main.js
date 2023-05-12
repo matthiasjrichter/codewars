77a20741295d50925f1e62d50f53a53ff0953a06
matthiasjrichter <draussensaiter@gmail.com>
2023-05-12 13:42:04 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -597,3 +597,32 @@ function cockroachSpeed(s) {
 }
 //short
 const cockroachSpeed = s => Math.floor(s * 27.777778)
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+// 
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function makeNegative(num) {
+  if(num > 0){
+    return num - 2 * num
+  }else {
+    return num
+  }
+}
+//shorter
+const makeNegative = num => num > 0 ? num - 2 * num : num
+//alternative
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+}
+//clever way
+function makeNegative(num) {
+  return -Math.abs(num);
+}

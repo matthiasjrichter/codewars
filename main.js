e187993ac49d45884f17ff0f0ff514451fb5d33a
matthiasjrichter <draussensaiter@gmail.com>
2023-05-13 15:57:56 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -626,3 +626,24 @@ function makeNegative(num) {
 function makeNegative(num) {
   return -Math.abs(num);
 }
+
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+// 
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+// 
+// For example, when the input is green, output should be yellow.
+function updateLight(current) {
+  switch (current) {
+    case "green":
+      return "yellow"
+      break;
+    case "yellow":
+      return "red"
+      break;
+    case "red":
+      return "green"
+      break;
+    default: "green"
+      break;
+  }
+}

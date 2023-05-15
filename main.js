ccae9fd12b7fb0153a74427e544f585ee22f4c05
matthiasjrichter <draussensaiter@gmail.com>
2023-05-15 05:07:06 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -640,3 +640,28 @@ function updateLight(current) {
 let stringToNumber = str => Number(str)
 //clever:
 // let stringToNumber = Number; //Number() is a built in function. so we just say that stringToNumber() should work the same!
+
+
+// I have a cat and a dog.
+// 
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+// 
+// Return their respective ages now as [humanYears,catYears,dogYears]
+// 
+// NOTES:
+// 
+// humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+function humanYearsCatYearsDogYears(humanYears) {
+  if(humanYears === 1) return [humanYears,15,15]
+  else if(humanYears === 2) return [humanYears,24,24]
+  else return [humanYears,24 + 4 * (humanYears - 2),24 + 5 * (humanYears - 2)]
+}

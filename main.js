9c5ef4efe210937737c719a95ea68cafa0721d02
matthiasjrichter <draussensaiter@gmail.com>
2023-05-20 03:37:43 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -757,3 +757,12 @@ function minMax(arr){
   return [Math.min(...arr), Math.max(...arr)]; //Math.min()
 }
 
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+// 
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+// 
+// None of the arrays will be empty, so you don't have to worry about that!
+
+
+

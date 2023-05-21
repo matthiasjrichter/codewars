70e53a2b8c7008312b1ff3e6fac9e653ed43aeba
matthiasjrichter <draussensaiter@gmail.com>
2023-05-21 07:56:13 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -775,3 +775,14 @@ function countSheeps(arrayOfSheep) {
 }
 //or
 let countSheeps = arrayOfSheep => arrayOfSheep.filter(sheep => sheep === true).length
+//
+function test(arr){
+  return arr.sort((a,b)=> a - b)
+}
+test([1,5,2])
+
+// Given an array of integers, return a new array with each value doubled.
+// 
+// For example:
+// 
+// [1, 2, 3] --> [2, 4, 6]

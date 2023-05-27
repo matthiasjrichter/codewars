c2399cd828577b54bd0732095ec8ed32c9b9c707
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2023-05-27 03:52:07 +0200

    new file:   getTheMeanOfAnArray.js

---
+++ getTheMeanOfAnArray.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/563e320cee5dddcf77000158/train/javascript
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+// 
+// Return the average of the given array rounded down to its nearest integer.
+// 
+// The array will never be empty.
+
+function getAverage(marks){
+    return Math.floor(marks.reduce((a, c) => a + c) / marks.length)
+  }
+
+  getAverage([1,2,3,4,5,])
+//
+const getAverage = marks => Math.floor(marks.reduce((a, c) => a + c) / marks.length)

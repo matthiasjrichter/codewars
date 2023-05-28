e66d24b05750699d5b47e68573993266151b8fb7
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2023-05-28 11:34:50 +0200

    added new file

---
+++ convertBoolean.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  if(bool = true) return "Yes"
+  else return "No"
+}
+//
+const boolToWord = bool => bool === true ? "Yes" : "No"
+//
+const boolToWord = bool => bool ? "Yes" : "No"

cd66a648ee3f5d81cc74387eed1af39ad8525ddd
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2023-07-07 03:04:32 +0200

    new file:   centuryFromYear.js

---
+++ centuryFromYear.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/javascript
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+// 
+// Task
+// Given a year, return the century it is in.
+// 
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+function century(year) {
+    if (String(year).slice(-2) === "00") return year / 100
+    else return Math.trunc(year / 100) + 1
+}
+
+//Solution with Math.ceil():
+const century = (year) => Math.ceil(year/100)

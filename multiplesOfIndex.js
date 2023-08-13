5f6be6b010d148e609a4d89e1dece31e380841a4
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2023-08-13 11:43:13 +0200

    new file:   multiplesOfIndex.js

---
+++ multiplesOfIndex.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/5a34b80155519e1a00000009/train/javascript
+// 
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+// 
+// Some cases:
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+// 
+// [68, -1, 1, -7, 10, 10] => [-1, 10]
+// 
+// [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+
+function multipleOfIndex(array) {
+    return array.filter((element, index) => element === 0 || element % index === 0)
+}
+
+//alternative
+const multipleOfIndex = (array) => array.filter((e, i) => e === 0 || e % i === 0)
+
+//test cases
+multipleOfIndex([22, -6, 32, 82, 9, 25]) //[-6, 32, 25]
+multipleOfIndex([-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]) //[-85, 72, 0, 68]

ba4163896616d345ea77891c7a08edeafe234b7a
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2023-08-22 02:48:12 +0200

    new file:   difOfVolsOfCuboids.js

---
+++ difOfVolsOfCuboids.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/58cb43f4256836ed95000f97/train/javascript
+// In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+// 
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+// 
+// Your function will be tested with pre-made examples as well as random ones.
+// 
+// If you can, try writing it in one line of code.
+
+function findDifference(a, b) {
+    let aVol = a[0] * a[1] * a[2]
+    let bVol = b[0] * b[1] * b[2]
+    return Math.abs(aVol - bVol)
+}
+
+findDifference([2, 2, 3], [5, 4, 1])
+
+//one line
+const findDifference = (a, b) => Math.abs(a[0] * a[1] * a[2] - b[0] * b[1] * b[2])

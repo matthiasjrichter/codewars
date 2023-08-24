9227e0e059c6b3746c8fcb0aa8fc832f6137f1b5
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2023-08-24 13:23:42 +0200

    new file:   isAnagram.js

---
+++ isAnagram.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/529eef7a9194e0cbc1000255/train/javascript
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+// 
+// Note: anagrams are case insensitive
+// 
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+// 
+// Examples
+// "foefet" is an anagram of "toffee"
+// 
+// "Buckethead" is an anagram of "DeathCubeK"
+
+function isAnagram(test, original) {
+    return  test
+            .toLowerCase()
+            .split("")
+            .sort()
+            .join()
+
+        === original
+            .toLowerCase()
+            .split("")
+            .sort()
+            .join()
+}
+
+//alternative
+
+const isAnagram = (test, original) =>
+    test
+        .toLowerCase()
+        .split("")
+        .sort()
+        .join()
+
+=== original
+        .toLowerCase()
+        .split("")
+        .sort()
+        .join()

c2ab5e604873f6b58aeee81a2cfae93d1e9cfaac
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2023-09-05 07:11:14 +0200

    new file:   reverseWords.js

---
+++ reverseWords.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/javascript
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+// 
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+    return str.split(" ").map(word => word.split("").reverse().join("")).join(" ");
+}
+
+//alternative
+const reverseWords = (str) => str.split(" ").map(word => word.split("").reverse().join("")).join(" ");

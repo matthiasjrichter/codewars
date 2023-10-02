8c88507801853b0e61a8013b8679ea13c8455339
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2023-10-02 09:06:21 +0200

    new file:   isThisMyTail.js

---
+++ isThisMyTail.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/56f695399400f5d9ef000af5/javascript
+// Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+//
+// If the tail is right return true, else return false.
+//
+// The arguments will always be non empty strings, and normal letters.
+//
+// function correctTail(bod, tail ;
+//
+//     sub = body.substr(bodylength-(tail.length)
+//
+//     if sub = tail) ;
+//       return true
+//     }
+//     else
+//       return false
+
+const correctTail = (body, tail) => body[body.length - 1] === tail;
+
+//alternative
+const correctTail = (body, tail) => body.endsWith(tail);

ff583757f5e3b91807fdee0ddfeb541e32a926d1
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2023-10-10 13:44:01 +0200

    new file:   binToDecimal.js

---
+++ binToDecimal.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/57a5c31ce298a7e6b7000334/train/javascript
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+// function binToDec(bin) {
+//   // TODO
+// }
+
+function binToDec(bin) {
+  return parseInt(bin, 2);
+}
+
+//alternative
+
+const binToDec = (bin) => parseInt(bin, 2);

7ecb8cc3180beb30307b220d028c345dcb680ae5
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2023-10-27 06:23:17 +0200

    new file:   exclamMarks7.js

---
+++ exclamMarks7.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/57fafb6d2b5314c839000195
+//
+// Description:
+// Remove words from the sentence if they contain exactly one exclamation mark. Words are separated by a single space, without leading/trailing spaces.
+//
+// Examples
+// remove("Hi!") === ""
+// remove("Hi! Hi!") === ""
+// remove("Hi! Hi! Hi!") === ""
+// remove("Hi Hi! Hi!") === "Hi"
+// remove("Hi! !Hi Hi!") === ""
+// remove("Hi! Hi!! Hi!") === "Hi!!"
+// remove("Hi! !Hi! Hi!") === "!Hi!"
+//
+// function remove (string) {
+//   //coding and coding....
+//   return '';
+// }
+
+function remove(string) {
+  return string
+    .split(" ")
+    .filter(
+      (word) =>
+        !word.includes("!") || word.indexOf("!") !== word.lastIndexOf("!")
+    )
+    .join(" ");
+}
+
+//alternative
+const remove = (string) => string.split(' ').filter(word => !word.includes('!') || word.indexOf('!') !== word.lastIndexOf('!')).join(' ');
+
+//test cases
+remove("Hi!"); //=== ""
+remove("Hi! Hi!"); //=== ""
+remove("Hi! Hi! Hi!"); //=== ""
+remove("Hi Hi! Hi!"); //=== "Hi"
+remove("Hi! !Hi Hi!"); //=== ""
+remove("Hi! Hi!! Hi!"); //=== "Hi!!"
+remove("Hi! !Hi! Hi!"); //=== "!Hi!"

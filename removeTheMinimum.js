8a931965be33c9a7af733801385e516adc4a9cfe
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2023-11-29 13:39:17 +0100

    new file:   removeTheMinimum.js

---
+++ removeTheMinimum.js
@@ -0,0 +1,73 @@
+// https://www.codewars.com/kata/563cf89eb4747c5fb100001b/train/javascript
+// The museum of incredible dull things
+// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+//
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+//
+// Task
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+//
+// Don't change the order of the elements that are left.
+//
+// Examples
+// * Input: [1,2,3,4,5], output = [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+//
+// function removeSmallest(numbers) {
+//     return [];
+//   }
+
+function removeSmallest(numbers) {
+  let result = Array.from(numbers);
+  let smallestNum = Math.min(...result);
+  let smallestNumIdx = result.findIndex((e) => e === smallestNum);
+  result.splice(smallestNumIdx, 1);
+  return result;
+}
+
+
+//with .indexOf() and spread.
+function removeSmallest(numbers) {
+  let result = [...numbers];
+  let smallestNum = Math.min(...result);
+  let smallestNumIdx = result.indexOf(smallestNum);
+  result.splice(smallestNumIdx, 1);
+  return result;
+}
+
+//test
+removeSmallest([2, 2, 1, 2, 1]); //[2,2,2,1]
+
+
+//shorter
+function removeSmallest(numbers) {
+  let result = [...numbers];
+  let smallestNum = Math.min(...result);
+  result.splice(result.indexOf(smallestNum), 1);
+  return result;
+}
+
+//test
+removeSmallest([2, 2, 1, 2, 1]); //[2,2,2,1]
+
+
+//even shorter
+function removeSmallest(numbers) {
+  let result = [...numbers];
+  result.splice(result.indexOf(Math.min(...result)), 1);
+  return result;
+}
+
+//test
+removeSmallest([2, 2, 1, 2, 1]); //[2,2,2,1]
+
+
+//with .filter()
+function removeSmallest(numbers) {
+  let smallestNum = Math.min(...numbers);
+  return numbers.filter((e, i) => i !== numbers.indexOf(smallestNum));
+}
+
+//test
+removeSmallest([2, 2, 1, 2, 1]); //[2,2,2,1]

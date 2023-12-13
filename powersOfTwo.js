2bfe8ecfb3a5ebd4aa1b09d2fe4ed4dea40e6a85
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2023-12-13 04:38:09 +0100

    new file:   powersOfTwo.js

---
+++ powersOfTwo.js
@@ -0,0 +1,44 @@
+// https://www.codewars.com/kata/57a083a57cb1f31db7000028
+//
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+//
+// Examples
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+//
+// function powersOfTwo(n){
+//     return []
+//   }
+//
+/////////
+
+function powersOfTwo(n) {
+  let result = [];
+  for (let i = 0; i <= n; i++) {
+    result.push(Math.pow(2, i));
+  }
+  return result;
+}
+
+powersOfTwo(2); //==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+//while loop
+function powersOfTwo(n) {
+  let result = [];
+  let i = 0;
+  while (i <= n) {
+    result.push(Math.pow(2, i));
+    i++;
+  }
+  return result;
+}
+
+//Array.from()
+const powersOfTwo = (n) => Array.from({ length: n + 1 }, (_, i) => Math.pow(2, i));
+
+//alternative
+const powersOfTwo = (n) => Array.from({ length: n + 1 }, (_, i) => 2 ** i);
+
+//test
+powersOfTwo(2); //==> [1, 2, 4]  # [2^0, 2^1, 2^2]

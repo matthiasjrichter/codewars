2315524ed9b019d5cdbfc7f8c4a190e65544e15c
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2024-01-06 08:34:29 +0100

    new file:   greetMe.js

---
+++ greetMe.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/535474308bb336c9980006f2
+//
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+//
+// Example:
+//
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+//
+// var greet = function(name) {
+//
+// };
+
+var greet = function (name) {
+  return `Hello ${name[0].toUpperCase() + name.toLowerCase().slice(1)}!`;
+};
+
+//alternative
+const greet = (name) => `Hello ${name[0].toUpperCase() + name.toLowerCase().slice(1)}!`;
+
+greet("riley"); //"Hello Riley!"

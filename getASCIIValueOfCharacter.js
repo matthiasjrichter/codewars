42d70d0023defd51db10c1a85d7881fc3bee397f
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2024-01-21 05:11:18 +0100

    new file:   getASCIIValueOfCharacter.js

---
+++ getASCIIValueOfCharacter.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/55acfc59c3c23d230f00006d
+//
+// Get ASCII value of a character.
+//
+// For the ASCII table you can refer to http://www.asciitable.com/
+//
+// function getASCII(c){
+//     // ...
+//   }
+
+function getASCII(c) {
+  return c.charCodeAt(0);
+}
+
+//arrow function
+const getASCII = (c) => c.charCodeAt(0);

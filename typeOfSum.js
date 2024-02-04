a1fbf685f830aaaced0034a49131a6a077ddef1c
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2024-02-04 04:33:43 +0100

    new file:   typeOfSum.js

---
+++ typeOfSum.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5a2e9ae2b6cfd7692a0000ba/
+//
+// Return the type of the sum of the two arguments
+//
+// function typeOfSum(a, b) {
+//   // good luck
+// }
+
+function typeOfSum(a, b) {
+  return typeof (a + b);
+}
+
+//arrow function
+const typeOfSum = (a, b) => typeof (a + b);

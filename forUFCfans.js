d562b6919b88c74cc48b293d36d60d529e72414c
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2024-02-10 10:10:54 +0100

    new file:   forUFCfans.js

---
+++ forUFCfans.js
@@ -0,0 +1,37 @@
+// https://www.codewars.com/kata/582dafb611d576b745000b74
+//
+// This is a beginner friendly kata especially for UFC/MMA fans.
+//
+// It's a fight between the two legends: Conor McGregor vs George Saint Pierre in Madison Square Garden. Only one fighter will remain standing, and after the fight in an interview with Joe Rogan the winner will make his legendary statement. It's your job to return the right statement depending on the winner!
+//
+// If the winner is George Saint Pierre he will obviously say:
+//
+// "I am not impressed by your performance."
+// If the winner is Conor McGregor he will most undoubtedly say:
+//
+// "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+// Good Luck!
+//
+// Note
+// The given name may varies in casing, eg., it can be "George Saint Pierre" or "geOrGe saiNT pieRRE". Your solution should treat both as the same thing (case-insensitive).
+//
+// var quote = function(fighter) {
+//     // your code here
+//   };
+
+var quote = function (fighter) {
+  if (fighter.toLowerCase() === "george saint pierre") return "I am not impressed by your performance.";
+  return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
+
+//arrow function
+const quote = (fighter) => fighter.toLowerCase() === "george saint pierre" ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+
+//a bit of fun
+const quote = (fighter) => fighter.toLowerCase().includes("c") ? "I'd like to take this chance to apologize.. To absolutely NOBODY!" : "I am not impressed by your performance.";
+
+//some Regex
+const quote = (fighter) => /c/i.test(fighter) ? "I'd like to take this chance to apologize.. To absolutely NOBODY!" : "I am not impressed by your performance.";
+
+//test
+quote("Conor McGregor") //"I'd like to take this chance to apologize.. To absolutely NOBODY!"

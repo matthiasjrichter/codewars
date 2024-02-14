aa25617bd85e949e454aa1f6ea73ec30bd4af49f
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2024-02-14 10:09:10 +0100

    new file:   contamination1string.js

---
+++ contamination1string.js
@@ -0,0 +1,30 @@
+// https://www.codewars.com/kata/596fba44963025c878000039
+// 
+// An AI has infected a text with a character!!
+// 
+// This text is now fully mutated to this character.
+// 
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+// 
+// Note: The character is a string of length 1 or an empty string.
+// 
+// Example
+// text before = "abc"
+// character   = "z"
+// text after  = "zzz"
+// 
+// function contamination(text, char){
+//     // Code here ;)
+//   }
+
+function contamination(text, char){
+    return char.repeat(text.length)
+  }
+
+//arrow function
+const contamination = (text, char) => char.repeat(text.length)
+
+
+//test
+contamination("abc","z") // "zzz"

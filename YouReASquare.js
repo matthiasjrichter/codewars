90447ea044d323d2c0156fc3f993ac45ac02755e
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2024-06-11 09:18:32 +0200

    Update YouReASquare.js

--- YouReASquare.js
+++ YouReASquare.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
+// https://www.codewars.com/kata/54c27a33fb7da0db0100040e
 // A square of squares
 // You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
 // 

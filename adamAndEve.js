194ff6284dc014ecd999554bf08ea0b917abcf2a
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2024-07-18 17:14:15 +0200

    modified:   adamAndEve.js

--- adamAndEve.js
+++ adamAndEve.js
@@ -1,9 +1,9 @@
 // https://www.codewars.com/kata/547274e24481cfc469000416
-// 
+//
 // According to the creation myths of the Abrahamic religions, Adam and Eve were the first Humans to wander the Earth.
-// 
+//
 // You have to do God's job. The creation method must return an array of length 2 containing objects (representing Adam and Eve). The first object in the array should be an instance of the class Man. The second should be an instance of the class Woman. Both objects have to be subclasses of Human. Your job is to implement the Human, Man and Woman classes.
-// 
+//
 // class God{
 //     /**
 //      * @returns Human[]
@@ -14,30 +14,14 @@
 //     }
 //     // code
 
-class Human {
-    constructor(name) {
-        this.name = name;
-    }
-}
-
-class Man extends Human {
-    constructor(name) {
-        super(name);
-        this.gender = 'male';
-    }
-}
-
-class Woman extends Human {
-    constructor(name) {
-        super(name);
-        this.gender = 'female';
-    }
-}
-
 class God {
-    static create() {
-        const adam = new Man('Adam');
-        const eve = new Woman('Eve');
-        return [adam, eve];
-    }
+  /**
+   * @returns Human[]
+   */
+  static create() {
+    return [new Man(), new Woman()];
+  }
 }
+class Human {}
+class Man extends Human {}
+class Woman extends Human {}

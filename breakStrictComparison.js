d7c84a08b53418689483901e354610013425d425
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2025-01-01 14:00:11 +0100

    new file:   breakStrictComparison.js

---
+++ breakStrictComparison.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/560f8d41cf6e1fe5c900002e
+//
+// In JavaScript, there is a special case where strict comparison of the same variable returns false! Try to find out what must be done to get such result!
+//
+// var x = something;
+// x === x // returns false!
+// Write a function which will return value for which strict comparison will give false!
+//
+//
+// function findStrangeValue() {
+//     // your code!
+//   }
+
+function findStrangeValue() {
+  return NaN;
+}
+
+// arrow function
+const findStrangeValue = () => NaN;

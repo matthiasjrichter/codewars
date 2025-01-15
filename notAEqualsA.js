4437c7aae0592cbae1463f6fa9423b638d3ceda4
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2025-01-15 15:31:58 +0100

    new file:   notAEqualsA.js

---
+++ notAEqualsA.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/59f9796cffe75f9299000025
+//
+// You task to pass only this tests :
+//
+// a == false
+// !a == false
+// a == !a
+//
+// const a =
+
+const a = new Boolean(false);

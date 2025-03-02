c1ad682444c5e5dcd741dc6b43a00b7e1afa9c6c
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2025-03-02 14:12:37 +0100

    new file:   dontGiveMeFive.js

---
+++ dontGiveMeFive.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/5813d19765d81c592200001a
+//
+// Don't give me five!
+// In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+//
+// Examples:
+//
+// 1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+// 4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+// The result may contain fives. ;-)
+// The start number will always be smaller than the end number. Both numbers can be also negative!
+//
+// I'm very curious for your solutions and the way you solve it. Maybe someone of you will find an easy pure mathematics solution.
+//
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+//
+// I have also created other katas. Take a look if you enjoyed this kata!
+//
+// function dontGiveMeFive(start, end)
+// {
+//   return 0;
+// }
+
+function dontGiveMeFive(start, end) {
+    let count = 0;
+    for (let number = start; number <= end; number++) {
+        if (!String(number).includes("5")) {
+            count++;
+        }
+    }
+    return count;
+}
+
+// alternative
+function dontGiveMeFive(start, end) {
+    return Array.from({ length: end - start + 1 }, (_, i) => i + start).filter(num => !String(num).includes("5")).length;
+}
+
+// test
+dontGiveMeFive(4,17) //  -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12

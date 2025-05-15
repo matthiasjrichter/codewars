21978ce9d294b8d01c944bfc3c5282a42c1d61ff
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2025-05-15 15:35:30 +0200

    new file:   getKeyValuePairsAsArrays.js

---
+++ getKeyValuePairsAsArrays.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/515dfd2f1db09667a0000003
+// 
+// Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+// 
+// Example:
+// 
+// keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]]
+// Style Points (JS/CoffeeScript only): This kata only tests for data that uses object literal notation (simple objects). For extra style, can you get your method to check for objects that extend their prototype?
+// 
+// function keysAndValues(data){
+//   // TODO: complete
+// }
+
+const keysAndValues = (data) => [Object.keys(data), Object.values(data)];

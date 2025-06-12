07577f0ca1df8fbc3881b1b704e68859cce760c7
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2025-06-12 09:15:02 +0200

    new file:   reverseList.js

---
+++ reverseList.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/57a04da9e298a7ee43000111
+// 
+// Write reverseList function that simply reverses lists.
+// 
+// function reverseList(arr) {
+//   return arr;
+// }
+
+const reverseList = (arr) => arr.reverse();

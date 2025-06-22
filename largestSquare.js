79f5c246d7d5462f50aac75da4897c0b04455a61
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2025-06-22 18:10:42 +0200

    new file:   largestSquare.js

---
+++ largestSquare.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5887a6fe0cfe64850800161c
+//
+// Determine the area of the largest square that can fit inside a circle with radius r.
+//
+// function areaLargestSquare(r) {
+//   return ; //code here
+// }
+
+function areaLargestSquare(r) {
+  return 2 * r * r;
+}
+// Arrow function
+const areaLargestSquare = (r) => 2 * r * r;

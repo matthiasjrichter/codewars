f59d9d61b7e1d43054a6dcf43f2137f02f8b92bf
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2025-06-23 18:49:15 +0200

    new file:   perimeterSequence.js

---
+++ perimeterSequence.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/589519d1f0902e01af000054/
+//
+// Perimeter sequence
+// The first three stages of a sequence are shown.
+//
+// blocks
+//
+// The blocksize is a by a and a ≥ 1.
+//
+// What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+//
+// function perimeterSequence(a,n) {
+//   // your code
+// }
+
+const perimeterSequence = (a, n) => 4 * a * n;

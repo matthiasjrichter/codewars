d419328a7842e37b2f558972becee65ea16cadc5
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2025-06-25 12:44:41 +0200

    new file:   thinkingAndTestingAB.js

---
+++ thinkingAndTestingAB.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/56d904db9963e9cf5000037d/
+// 
+// No Story
+// 
+// No Description
+// 
+// Only by Thinking and Testing
+// 
+// Look at result of testcase, guess the code!
+// 
+// function testit(a,b){
+//   //return a+b ?
+//   //return a*b ?
+//   return a?b;
+// }
+
+const testit = (a, b) => a | b;

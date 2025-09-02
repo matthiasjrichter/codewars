273855428a2cbba27837ab6096e01fecf01ab6e1
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2025-09-02 11:06:05 +0200

    new file:   areaOfAnArrow.js

---
+++ areaOfAnArrow.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/589478160c0f8a40870000bc
+// 
+// An arrow is formed in a rectangle with sides a and b by joining the bottom corners to the midpoint of the top edge and the centre of the rectangle.
+// 
+// a and b are integers and > 0
+// 
+// Write a function which returns the area of the arrow.
+// 
+// function arrowArea(a,b) {
+//   // your code
+// }
+
+const arrowArea = (a, b) => a * b / 4;

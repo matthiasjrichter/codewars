5e9e134ec116dd6feb6870498be9f61f09adf4b1
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2025-10-10 08:09:21 +0200

    new file

---
+++ BRLCurrencyFormat.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/5835e6ce6bdc270a5f00019b
+// 
+// Format a number as BRL money, i.e. R$ 10,00
+// 
+// More examples
+// 
+// currencyBRL(10) => 'R$ 10,00'
+// currencyBRL(10.5) => 'R$ 10,50'
+// currencyBRL(10.05) => 'R$ 10,05'
+// currencyBRL(.5) => 'R$ 0,50'
+// The values must be rounded to the nearest value, like:
+// 
+// currencyBRL(10.505) => 'R$ 10,51'
+// currencyBRL(10.504) => 'R$ 10,50'
+// currencyBRL(10.502) => 'R$ 10,50'
+// currencyBRL(10.509) => 'R$ 10,51'
+// 
+// function currencyBRL(number) {
+//   return number
+// }
+
+const currencyBRL = (number) => 'R$ ' + (number || 0).toFixed(2).replace('.', ',');
+

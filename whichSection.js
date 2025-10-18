2ddd5b978a224090e52f59c66f04612bca18a3ea
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2025-10-18 08:19:51 +0200

    new file:   whichSection.js

---
+++ whichSection.js
@@ -0,0 +1,34 @@
+// https://www.codewars.com/kata/5cb05eee03c3ff002153d4ef/
+//
+// Your website is divided vertically in sections, and each can be of different size (height).
+// You need to establish the section index (starting at 0) you are at, given the scrollY and sizes of all sections.
+// Sections start with 0, so if first section is 200 high, it takes 0-199 "pixels" and second starts at 200.
+//
+// Example:
+// With scrollY = 300 and sizes = [300,200,400,600,100]
+//
+// the result will be 1 as it's the second section.
+//
+// With scrollY = 1600 and size = [300,200,400,600,100]
+//
+// the result will be -1 as it's past last section.
+//
+// Given the scrollY integer (always non-negative) and an array of non-negative integers (with at least one element), calculate the index (starting at 0) or -1 if scrollY falls beyond last section (indication of an error).
+//
+// // return an index of section or -1 if past last section
+// // scrollY is a positive integer and sizes is an array of positive integers
+// function getSectionIdFromScroll(scrollY,sizes){
+//
+// }
+
+function getSectionIdFromScroll(scrollY, sizes) {
+  let currentY = 0;
+  for (let i = 0; i < sizes.length; i++) {
+    currentY += sizes[i];
+    if (scrollY < currentY) return i;
+  }
+  return -1;
+}
+
+// .findIndex()
+const getSectionIdFromScroll = (scrollY, sizes, sum = 0) => sizes.findIndex((e) => (sum += e) > scrollY);

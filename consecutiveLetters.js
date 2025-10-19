7ff53ec5ff080a7b0f847ad6278695020456e76c
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2025-10-19 08:38:25 +0200

    new file:   consecutiveLetters.js

---
+++ consecutiveLetters.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/5ce6728c939bf80029988b57/
+//
+// In this Kata, we will check if a string contains consecutive letters as they appear in the English alphabet and if each letter occurs only once.
+//
+// Rules are: (1) the letters are adjacent in the English alphabet, and (2) each letter occurs only once.
+//
+// For example:
+// solve("abc") = True, because it contains a,b,c
+// solve("abd") = False, because a, b, d are not consecutive/adjacent in the alphabet, and c is missing.
+// solve("dabc") = True, because it contains a, b, c, d
+// solve("abbc") = False, because b does not occur once.
+// solve("v") = True
+// All inputs will be lowercase letters.
+//
+// More examples in test cases. Good luck!
+//
+// function solve(s){
+//     //..
+// }
+
+function solve(s) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  return alphabet.includes([...s].sort().join(""));
+}
+
+// One line for fun
+const solve = (s) => "abcdefghijklmnopqrstuvwxyz".includes([...s].sort().join(""));

5f9ede7d9058b07208cb66101af3ee5c82774c9d
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2025-11-01 18:44:48 +0100

    new file:   sumOfFirstNthTermOfSeries.js

---
+++ sumOfFirstNthTermOfSeries.js
@@ -0,0 +1,49 @@
+// https://www.codewars.com/kata/555eded1ad94b00403000071/
+//
+// Task
+// Your task is to write a function which returns the n-th term of the following series, which is the sum of the first n terms of the sequence (n is the input parameter).
+//
+// You will need to figure out the rule of the series to complete this.
+//
+// Rules
+// You need to round the answer to 2 decimal places and return it as String.
+//
+// If the given value is 0 then it should return "0.00".
+//
+// You will only be given Natural Numbers as arguments.
+//
+// Examples (Input --> Output)
+// n
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+//
+// function SeriesSum(n) {
+//   // Happy Coding ^_^
+// }
+
+function SeriesSum(n) {
+  if (n === 0) return "0.00";
+
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum += 1 / (1 + i * 3);
+  }
+
+  return sum.toFixed(2);
+}
+
+// .reduce()
+function SeriesSum(n) {
+  if (n === 0) return "0.00";
+
+  const sum = Array.from({ length: n }, (_, i) => 1 / (1 + i * 3)).reduce((a, c) => a + c, 0);
+
+  return sum.toFixed(2);
+}
+
+// One line for fun:
+const SeriesSum = (n) => n ? Array.from({ length: n }, (_, i) => 1 / (1 + i * 3)).reduce((acc, val) => acc + val, 0).toFixed(2) : "0.00";
+
+// Spread
+const SeriesSum = (n) => n ? [...Array(n)].reduce((a, _, i) => a + 1 / (1 + i * 3), 0).toFixed(2) : "0.00";

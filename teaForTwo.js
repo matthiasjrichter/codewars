0021e2ea4a72f02b63397eaaf1d0378e32c23a82
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2026-03-02 09:46:07 +0100

    new file:   teaForTwo.js

---
+++ teaForTwo.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/555a7dc335d4c407af00006e
+//
+// Tea for two!
+//
+// Given a string or number write a function tea42 that takes as input a string or number and returns a string where every '2' charcter or digit has been replaced with a t.
+//
+// tea42('coffee') //should return 'coffee'
+// tea42('tea') //should return 'tea'
+// tea42('2ea') //should return 'tea'
+// tea42(9022) //should return '90tt'
+// tea42(5676765) //should return '5676765'
+// tea42('2u2u') //should return 'tutu'
+//
+// function tea42(input) {
+//   // your code
+// };
+
+function tea42(input) {
+  return String(input).replaceAll("2", "t");
+}
+
+// Arrow function
+const tea42 = input => String(input).replaceAll("2", "t");

56a2b56b876c86f2d1e5c00954e22f180d9ff5aa
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2026-03-13 09:53:12 +0100

    new file:   lettersOnly.js

---
+++ lettersOnly.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/59be6bdc4f98a8a9c700007d
+//
+// Let's assume we need "clean" strings. Clean means a string should only contain letters a-z, A-Z and spaces. We assume that there are no double spaces or line breaks.
+//
+// Write a function that takes a string and returns a string without the unnecessary characters.
+//
+// Examples
+// '.tree1'                     ==> 'tree'
+// "that's a pie$ce o_f p#ie!"  ==> 'thats a piece of pie'
+// 'john.dope@dopington.com'    ==> 'johndopedopingtoncom'
+// 'my_list = ["a","b","c"]'    ==> 'mylist  abc'
+// '1 + 1 = 2'                  ==> '    '
+// "0123456789.+,|[]{}=@/~;^$'<>?-!*&:#%_"  ==> ''
+//
+// function removeChars(s) {
+//
+// }
+
+function removeChars(s) {
+  const lettersAndSpace = /[^a-z ]/gi;
+  return s.replace(lettersAndSpace, "");
+}
+
+// Arrow function
+const removeChars = s => s.replace(/[^a-z ]/gi, "");

6be04289237d3989edfe517d7f132f119d5c71d2
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2026-03-22 18:48:00 +0100

    new file: whichStringIsWorthMore.js

---
+++ whichStringIsWorthMore.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5840586b5225616069000001
+// 
+// You will be given two ASCII strings, a and b. Your task is write a function to determine which one of these strings is "worth" more, and return it.
+// 
+// A string's worth is determined by the sum of its ASCII codepoint indexes. So, for example, the string HELLO has a value of 372: H is codepoint 72, E 69, L 76, and O is 79. The sum of these values is 372.
+// 
+// In the event of a tie, you should return the first string, i.e. a.
+// 
+// function highestValue(a, b) {
+//   
+// }
+
+const getValue = s => [...s].reduce((a, c) => a + c.charCodeAt(0), 0);
+const highestValue = (a, b) => getValue(a) >= getValue(b) ? a : b;
